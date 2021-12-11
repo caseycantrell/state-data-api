31206a4f7af168be69b41f29155ec34e270a3812
Casey <cantrellcasey@gmail.com>
2021-12-10 19:27:04 -0600

    generates states controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,13 @@ require 'csv'
 #   })
 # end
 
-CSV.foreach(Rails.root.join('lib/states.csv'), headers: true) do |row|
+# CSV.foreach(Rails.root.join('lib/states.csv'), headers: true) do |row|
   
-  State.create({
-    state: row[0],
-    median_household_income: row[1],
-    share_unemployed_seasonal: row[2],
-    share_population_in_metro_areas: row[3],
-    share_population_with_high_school_degree: row[4]
-  })
-end
+#   State.create({
+#     state: row[0],
+#     median_household_income: row[1],
+#     share_unemployed_seasonal: row[2],
+#     share_population_in_metro_areas: row[3],
+#     share_population_with_high_school_degree: row[4]
+#   })
+# end
